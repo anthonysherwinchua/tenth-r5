e1b470c125a3b404ffdb629f3527ce29dfd9f9c8
anthonysherwinchua <anthonysherwinchua@gmail.com>
2017-04-26 01:50:19 +0800

    add user seed file

--- db/seeds/users.rb
+++ db/seeds/users.rb
@@ -1,6 +1,6 @@
 User.create([
-             { email: "admin@10thstory.com", password: 'password', role: :admin },
-             { email: "manager@10thstory.com", password: 'password', role: :manager },
-             { email: "cashier@10thstory.com", password: 'password', role: :cashier },
-             { email: "processing@10thstory.com", password: 'password', role: :processing }
+             { email: "admin@10thstory.com", username: "admin@10thstory.com", password: 'password123', role: :admin },
+             { email: "manager@10thstory.com", username: "manager@10thstory.com", password: 'password123', role: :manager },
+             { email: "cashier@10thstory.com", username: "cashier@10thstory.com", password: 'password123', role: :cashier },
+             { email: "processing@10thstory.com", username: "processing@10thstory.com", password: 'password123', role: :processing }
            ])
