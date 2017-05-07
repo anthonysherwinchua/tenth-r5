f173d1a6355dcd33ef04bdfcce25eef87fc8a19b
anthonysherwinchua <anthonysherwinchua@gmail.com>
2017-05-08 00:55:35 +0800

    add spec for blood type ctrl

--- spec/routings/admin/lookups/blood_types_controller_routing_spec.rb
+++ spec/routings/admin/lookups/blood_types_controller_routing_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Admin::Lookups::BloodTypesController, type: :routing do
   it { expect(get: '/admin/lookups/blood_types').to route_to('admin/lookups/blood_types#index') }
 
   it { expect(get: '/admin/lookups/blood_types/new').to route_to('admin/lookups/blood_types#new') }
+  it { expect(get: '/admin/lookups/blood_types/new').to route_to('admin/lookups/blood_types#new') }
   it { expect(post: '/admin/lookups/blood_types').to route_to('admin/lookups/blood_types#create') }
 
   it { expect(get: '/admin/lookups/blood_types/1/edit').to route_to('admin/lookups/blood_types#edit', id: '1') }
